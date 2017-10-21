d3c48edabd3e491d092db104c2e107ebe92614ac
Bishaka Samuel <sbishaka@gmail.com>
2017-10-21 08:45:37 +0300

    remove staging folder

--- staging/db.js
+++
@@ -1,9 +0,0 @@
-(function(window){
-    var db = new Dexie("genki_database");
-    var dbVersion = 1;
-    db.version(dbVersion).stores({
-        docs: 'id,name,cards,activeCardId,lastModified',
-        meta: 'name,value,lastModified'
-    });   
-    window.db = db;
-}(window))
